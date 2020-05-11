eb40a23b9fd1c8e838018200f8d3ec52eac7ed4b
Marco Silvestri <m.silvestri03@gmail.com>
2020-05-11 17:04:30 +0200

    Add + refresh

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+
+    /* References */
+
+    var buttonSubmit = $('i.fas.fa-plus');
+    var inputArea = $('.add-task');
+    var list = $('.todo');
+    var dateDay = $('.calendar__day');
+    var dateName = $('.calendar__name');
+    var dateMonth = $('.calendar__month');
+    var btnUpdate = $('i.fas.fa-sync-alt');
+    var btnRemove = $('i.fas.fa-minus')
+    
+    var url = 'http://157.230.17.132:3022/todos';
+
+    //Handlebars
+    var source = $("#list__element-template").html();
+    var template = Handlebars.compile(source);
+
+
+    //Add the note on the click of the + button
+    buttonSubmit.click(function(){
+        addAPI(url, inputArea.val(), template, list);
+    });
+
+    // Add the new note on the hit of ENTER
+    inputArea.keyup(function(e){
+        if (e.which == 13 || e.keyCode == 13){//13 is ENTER
+            addAPI(url, inputArea.val(), template, list);;
+        }
+    });
+
+    // Refresh
+    btnUpdate.click(function () { 
+        updateAPI(callGet,template,list);
+    });
+
+}); //END of DOCUMENT
+
+    /****************
+    *  Functions
+    *****************/
+function updateAPI(url, template, destination){
+    var settings = {
+        url : url,
+        method : 'GET'
+    };
+    $.ajax(settings).done(function(result){
+        destination.html('');
+        printTodos(result, template, destination);
+    }).fail(function(error){
+        console.log('Error #' + error.status);
+    });
+}
+
+function addAPI(url, input, template, destination){
+    var settings = {
+        url : url,
+        method : 'POST',
+        data : {text : input}
+    };
+    $.ajax(settings).done(function(){
+        updateAPI(url, template, destination);
+    }).fail(function(error){
+        console.log('Error #' + error.status);
+    });
+}
+
+function printTodos(data, template, destination){
+    for (var i = 0; i < data.length; i++){
+        var templateData = {
+            todoId : data[i].id,
+            todoElement : data[i].text
+        };
+        var output = template(templateData);
+        destination.append(output);
+    }
+}
